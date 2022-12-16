4d96bd8667d9877a0fed58ce19b4db271ca78fab
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2022-12-16 01:59:58 -0500

    remove shm unnecessary code

--- shm/shm.h
+++ shm/shm.h
@@ -1,5 +1,5 @@
-#ifndef __GO_BBS_SHM_H__
-#define __GO_BBS_SHM_H__
+#ifndef __GO_PTTBBS_SHM_H__
+#define __GO_PTTBBS_SHM_H__
 
 #include <errno.h>
 #include <stdio.h>
@@ -7,27 +7,4 @@
 #include <string.h>
 #include <sys/shm.h>
 
-#define OFFSET_BOARD_HEADER_BRDNAME 0  // brd-name: IDLEN+1 (12+1)
-#define SIZE_BOARD_HEADER_BRDNAME 13
-
-#define OFFSET_BOARD_HEADER_TITLE 13  // title: BTLEN+1 (48+1)
-#define SIZE_BOARD_HEADER_TITLE 49
-
-#define SIZE_BOARD_HEADER 256
-
-// XXX consider changing offset to long long? (64bit?)
-int isEExist();
-int isPtrLessThan0(void *p);
-// void readwrapper(void *outptr, void *shmaddr, int offset, unsigned long n);
-// void writewrapper(void *shmaddr, int offset, void *inptr, unsigned long n);
-//  void set_or_uint32wrapper(void *shmaddr, int offset, unsigned int flag);
-//  void innerset_int32wrapper(void *shmaddr, int offsetSrc, int offsetDst);
-//  void memsetwrapper(void *shmaddr, int offset, unsigned char c, unsigned long n);
-
-// void set_bcacheptr(void *shmaddr, int offset);
-// void qsort_cmpboardname_wrapper(void *shmaddr, int offset, unsigned long n);
-// void qsort_cmpboardclass_wrapper(void *shmaddr, int offset, unsigned long n);
-// int cmpboardname(const void *i, const void *j);
-// int cmpboardclass(const void *i, const void *j);
-
-#endif  //__GO_BBS_SHM_H__
+#endif  //__GO_PTTBBS_SHM_H__
